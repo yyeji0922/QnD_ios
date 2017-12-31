6501135f5da2b72a9ec72d33852e69e6b9a56c88
yyeji0922 <yyeji0922@hanmail.net>
2017-12-31 13:07:39 +0900

    Storyboard용 파일 생성

---
+++ storyboard/QnD_storyboard/QnD_storyboard/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  QnD_storyboard
+//
+//  Created by 정예지 on 2017. 12. 31..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ storyboard/QnD_storyboard/QnD_storyboardTests/QnD_storyboardTests.swift
@@ -0,0 +1,35 @@
+//
+//  QnD_storyboardTests.swift
+//  QnD_storyboardTests
+//
+//  Created by 정예지 on 2017. 12. 31..
+//
+
+import XCTest
+@testable import QnD_storyboard
+
+class QnD_storyboardTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
