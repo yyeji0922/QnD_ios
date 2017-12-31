8c0de374e2e6b1eec3f447978ea3da68f8d34523
yyeji0922 <yyeji0922@hanmail.net>
2017-12-31 14:28:05 +0900

    QnD_xib 폴더 추가

---
+++ xib/QnD_xib/QnD_xib/firstViewController.swift
@@ -0,0 +1,34 @@
+//
+//  firstViewController.swift
+//  QnD_xib
+//
+//  Created by 정예지 on 2017. 12. 31..
+//
+
+import UIKit
+
+class firstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
